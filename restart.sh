a95639fde44dcaac4195af6f063660fe241caecb
Pavel Shumskii <spo@home13-server.localdomain>
2020-08-13 07:29:33 +0700

    add alpine container + review

--- restart.sh
+++ restart.sh
@@ -11,7 +11,9 @@ function question {
  [[ "${REPLY}" =~ ^([yY][eE][sS]|[yY])$ ]] && ${@}
 }
 
-q="Fix ${PWD} file permissions?"  question chcon -vRt svirt_sandbox_file_t . && chmod -vR o+rw .
-q='Stop sshd-container?'          question "${COMPOSE}" down && sleep 8s
-q='Build sshd-container?'         question "${COMPOSE}" build
-q='Start sshd-container?'         question "${COMPOSE}" up -d
+#q="Fix ${PWD} file permissions?"  question chcon -vRt svirt_sandbox_file_t . && chmod -vR o+rw .
+q='Stop sshd-container?'          question "${COMPOSE}" down  && sleep 8s
+q='Build sshd-container?'         question "${COMPOSE}" build || exit 1
+q='Start sshd-container?'         question "${COMPOSE}" up -d && sleep 8s
+
+q="Test sshd?"  question ssh root@localhost -p ${SSHD_EXTERNAL_PORT:-${SSHD_PORT}} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no true && echo 'Test Passed.'
