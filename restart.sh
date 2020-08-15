39b6dc9b5088ee2e4ae1e285ecc5d16501b9e7ad
Pavel Shumskii <spo@home13-server.localdomain>
2020-08-15 11:12:46 +0700

    fix compose-env.yaml run examples

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ pushd $(dirname $0)
 trap 'popd' EXIT
 
 source ./.env
-COMPOSE="./compose.yaml"
+COMPOSE="./compose.yaml -f ./compose-env.yaml"
 
 function question {
  read -r -p "${q:-1}: [y/N] "
@@ -12,8 +12,6 @@ function question {
 }
 
 #q="Fix ${PWD} file permissions?"  question chcon -vRt svirt_sandbox_file_t . && chmod -vR o+rw .
-q='Stop sshd-container?'          question "${COMPOSE}" down  && sleep 8s
-q='Build sshd-container?'         question "${COMPOSE}" build || exit 1
-q='Start sshd-container?'         question "${COMPOSE}" up -d && sleep 8s
-
-q="Test sshd?"  question ssh root@localhost -p ${SSHD_EXTERNAL_PORT:-${SSHD_PORT}} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no true && echo 'Test Passed.'
+q='Stop sshd?'  question "${COMPOSE}" down  && sleep 8s
+q='Start sshd?' question "${COMPOSE}" up -d && sleep 8s
+q='Test sshd?'  question ssh root@localhost -p ${SSHD_EXTERNAL_PORT:-${SSHD_PORT}} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no true && echo 'Test Passed.'
